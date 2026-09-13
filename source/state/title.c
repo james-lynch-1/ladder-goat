9472d6c66e08067a85e5323dbff14d0566b377b2
James Lynch <jly140.96@gmail.com>
2026-09-14 01:13:01 +1000

    Add my nom de plume to the title screen

--- source/state/title.c
+++ source/state/title.c
@@ -4,14 +4,15 @@
 
 void enterTitle() {
     REG_BG1VOFS = -80;
+    REG_BG1HOFS = -8;
     REG_DISPCNT &= ~(DCNT_BG0 | DCNT_OBJ);
     loadBG(UI_SBB, BG_UI, UI_PAL, ScratchLogoSmall1Pal, ScratchLogoSmall1PalLen,
         ScratchLogoSmall1Tiles, ScratchLogoSmall1TilesLen,
         ScratchLogoSmall1Map, ScratchLogoSmall1MapLen);
     tte_erase_screen();
-    tte_set_pos(8, 40);
-    tte_write("Untitled Puzzle Game with\n Two Controllable Characters\n and Three Levels, the Last\n of Which is Fairly Difficult");
-    tte_set_pos(8, 80);
+    tte_set_pos(8, 16);
+    tte_write("Brother Jauffre's\n Untitled Puzzle Game with\n Two Controllable Characters\n and Three Levels, the Last\n of Which is Fairly Difficult");
+    tte_set_pos(8, 72);
     tte_write("PRESS START");
 }
 
